51af8ae194ee8a7eeab268649a21a069c2e4d3c3
Akash Jha <akashxjha@gmail.com>
2025-06-27 23:30:11 +0530

    feat: update authentication layout and views for sign-in and sign-up

---
+++ src/app/auth/sign-up/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { Card } from "@/components/ui/card";
+import { SignUpView } from "@/modules/auth/ui/views/sign-up-view";
+const Page=()=>{
+    return(
+       <SignUpView/>
+          
+    )
+
+}
+
+export default Page;
+
