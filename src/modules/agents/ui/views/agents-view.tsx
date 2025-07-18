40e103b4e9fba2e6aa9585835c6c1485cfe98877
Akash Jha <akashxjha@gmail.com>
2025-07-18 17:33:41 +0530

    feat: add humanize-duration for meeting duration formatting; implement DataTable component for meetings view

--- src/modules/agents/ui/views/agents-view.tsx
+++ src/modules/agents/ui/views/agents-view.tsx
@@ -5,12 +5,12 @@ import { ResponsiveDialog } from "@/components/responsive-dailog";
 import { Button } from "@/components/ui/button";
 import { useTRPC } from "@/trpc/client"
 import { useQuery, useSuspenseQuery } from "@tanstack/react-query"
-import { DataTable } from "../components/data-table";
 import { columns } from "../components/columns";
 import { EmptyState } from "@/components/empty-state";
 import { useAgentsFilters } from "../../hooks/use-agents-filters";
 import { DataPagination } from "../components/data-pagination";
 import { useRouter } from "next/navigation";
+import { DataTable } from "@/components/data-table";
  export const AgentsView =()=>{
     const router = useRouter();
     const [filters,setFilters]= useAgentsFilters();
--- src/modules/meetings/server/procedures.ts
+++ src/modules/meetings/server/procedures.ts
@@ -1,9 +1,9 @@
 import { db } from "@/db";
-import { meetings } from "@/db/schema";
+import { agents, meetings } from "@/db/schema";
 import { createTRPCRouter, baseProcedure, protectedProcedure } from "@/trpc/init";
 import { TRPCError } from "@trpc/server";
 import { z } from "zod";
-import { count,and, eq, getTableColumns, ilike, desc } from "drizzle-orm";
+import { count,and, eq, getTableColumns, ilike, desc, sql } from "drizzle-orm";
 import { DEFAULT_PAGE, DEFAULT_PAGE_SIZE, MAX_PAGE_SIZE, MIN_PAGE_SIZE } from "@/constants";
 import { meetingsInsertSchema, meetingsUpdateSchema } from "../schemas";
 
@@ -85,9 +85,12 @@ const {search,page,pageSize}= input;
         .select({
            
             ...getTableColumns(meetings),
+            agent:agents,
+            duration:sql<number>`EXTRACT(EPOCH FROM(ended_at-started_at))`.as("duration"),
             
         })
         .from(meetings)
+        .innerJoin(agents, eq(meetings.agentId, agents.id))
         .where(
             and(
                 eq(meetings.userId, ctx.auth.user.id),
@@ -101,6 +104,7 @@ const {search,page,pageSize}= input;
         const [total] = await db
         .select({ count: count() })
         .from(meetings)
+        .innerJoin(agents, eq(meetings.agentId, agents.id))
         .where(
             and(
              eq(meetings.userId, ctx.auth.user.id),
